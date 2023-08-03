64fdd3c82a272c81022f843874263ec63f22ea7b
Tendai Choruwa <40615331@live.napier.ac.uk>
2023-08-03 08:25:58 +0200

    Testing github token usage

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,6 +3,6 @@ package com.napier.sem;
 public class App {
     public static void main (String[] args)
     {
-        System.out.println("Boo Yah!! It works hey");
+        System.out.println("Boo Yah!! It works hey-hip hip hooray");
     }
 }
